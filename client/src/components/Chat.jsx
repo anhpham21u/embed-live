e371f38c77f27567d87555c77a00cf21679c36ba
anhpham <hoctap31111@gmail.com>
2023-06-07 22:25:28 +0700

    chat

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,60 @@
+import styles from "./../components/chat.module.scss";
+import { Container, InputGroup, Button, Form } from "react-bootstrap";
+import { useEffect, useState, useRef } from "react";
+
+function Chat() {
+  const [chatLog, setChatLog] = useState([]);
+  const btnSend = useRef(null);
+  const input = useRef(null);
+
+  useEffect(() => {
+    const handleSend = () => {
+      const newChatLog = [...chatLog, input.current.value];
+      setChatLog(newChatLog);
+      input.current.value = "";
+    };
+
+    btnSend.current.addEventListener("click", handleSend);
+
+    return () => {
+      btnSend.current.removeEventListener("click", handleSend);
+    };
+  });
+
+  return (
+    <Container className="my-5">
+      <div className={styles.chatBox + " bg-light"}>
+        <h3 className={styles.head + " p-3"}>Chat Box</h3>
+
+        <div className={styles.chatBody + " p-3"}>
+          {chatLog.map((data, idx) => (
+            <div className="d-flex justify-content-end">
+              <p key={idx} className={styles.mySend + " p-2"}>
+                {data}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <InputGroup className="mb-3 ">
+          <Form.Control
+            placeholder="Write some here"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+            ref={input}
+          />
+          <Button
+            variant="secondary"
+            id="button-addon2"
+            size="lg"
+            ref={btnSend}
+          >
+            Gửi
+          </Button>
+        </InputGroup>
+      </div>
+    </Container>
+  );
+}
+
+export default Chat;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import MyNav from "../components/MyNav";
 import Video from "../components/Video";
+import Chat from "../components/Chat";
 
 function Home() {
   return (
     <div>
       <MyNav />
       <Video />
+      <Chat />
     </div>
   );
 }
